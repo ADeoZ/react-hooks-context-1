b78d96d9ce9a1fb393844f7cc3e43578c1df01d6
Aleksey Denisov <deoz@yandex.ru>
2021-08-19 21:38:38 +0300

    Fix

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types';
 export default function Details({ info }) {
   const [user, setUser] = useState();
   const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
 
   useEffect(() => {
     if (info) {
+      setError(false);
       setLoading(true);
       fetch(`${process.env.REACT_APP_DATA_URL}${info.id}.json`)
         .then((response) => response.json())
         .then((data) => {
           setUser(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(true);
           setLoading(false);
         });
     }
@@ -19,6 +25,7 @@ export default function Details({ info }) {
 
   return (
     <div className="Details">
+      {isError && <p>Ошибка загрузки</p>}
       {isLoading && <p>Loading...</p>}
       {!isLoading && user && (
         <div className="Details-container">
--- src/components/List.js
+++ src/components/List.js
@@ -3,15 +3,28 @@ import PropTypes from 'prop-types';
 
 export default function List({ selectUser }) {
   const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
 
   useEffect(() => {
+    setError(false);
+    setLoading(true);
     fetch(`${process.env.REACT_APP_DATA_URL}users.json`)
       .then((response) => response.json())
-      .then((data) => setUsers(data));
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(true);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <ul className="List">
+      {isError && <li className="List-item">Ошибка загрузки</li>}
+      {isLoading && <li className="List-item">Loading...</li>}
       {users.map((item) => {
         return <li key={item.id} className="List-item" onClick={selectUser(item)}>{item.name}</li>
       })}
